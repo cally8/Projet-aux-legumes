1f3f378a9a0620b387b6f7ab685ed37d05b08bc6
Cally_5 <josephsaida5@gmail.com>
2024-02-19 21:04:38 +0100

    Page panier de commande à faire

--- portofolio/Ficher_de_développement/proscript.js
+++ portofolio/Ficher_de_développement/proscript.js
@@ -1,33 +1,47 @@
-//Page principale: Acceuil//
+//Page principale: Acceuil
 
 
 
 
-//Commander//
+//Commander
 
 // Le saviez-vous 
+var culture = [];
 
-function afficherDetails() {
-  document.getElementById('laCulture1').style.display = 'flex';
-}
-function fermerLeContenu() {
-  document.getElementById('laCulture1').style.display = 'none';
-}
-
-function afficherDetails() {
-  document.getElementById('laCulture2').style.display = 'flex';
-}
-function fermerLeContenu() {
-  document.getElementById('laCulture2').style.display = 'none';
+function afficherDetails(produitId) {
+    var laCultureElement = document.getElementById('laCulture' + produitId.slice(-1));
+    laCultureElement.style.display = 'block';
 }
 
+function fermerLeContenu(produitId) {
+    var laCultureElement = document.getElementById('laCulture' + produitId.slice(-1));
+    laCultureElement.style.display = 'none';
+}
+//Panier de commande
+let openShopping = document.querySelector('.shopping');
+let fermerShopping = document.querySelector('.fermerShopping');
+let list = document.querySelector('.list');
+let listPanier= document.querySelector('.listPanier');
+let total= document.querySelector('.total');
+let header =document.querySelector('header');
+let main = document.querySelector('main')
+let quantité = document.querySelector('.quantité');
+
+let products = []
+
+// openShopping.addEventListener('click', ()=> {
+//     header.classList.add('active');
+// })
+// fermerShopping.addEventListener('click', ()=> {
+//     main.classList.remove('active');
+// })
 
 
+//Panier
+
+// Afficher les éléments de "Le saviez-vous"
 
 
-
-
-//Panier//
 
 // Nous-contacter//
 
