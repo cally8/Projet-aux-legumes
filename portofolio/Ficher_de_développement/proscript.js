f65fb2eb925f2e295fc32286281fd9b63de86d48
Cally_5 <josephsaida5@gmail.com>
2024-02-23 00:23:45 +0100

    Mise en place du panier

--- portofolio/Ficher_de_développement/proscript.js
+++ portofolio/Ficher_de_développement/proscript.js
@@ -4,7 +4,6 @@
 
 
 //Commander
-
 // Le saviez-vous 
 var culture = [];
 
@@ -17,24 +16,44 @@ function fermerLeContenu(produitId) {
     var laCultureElement = document.getElementById('laCulture' + produitId.slice(-1));
     laCultureElement.style.display = 'none';
 }
-//Panier de commande
-let openShopping = document.querySelector('.shopping');
-let fermerShopping = document.querySelector('.fermerShopping');
-let list = document.querySelector('.list');
-let listPanier= document.querySelector('.listPanier');
-let total= document.querySelector('.total');
-let header =document.querySelector('header');
-let main = document.querySelector('main')
-let quantité = document.querySelector('.quantité');
+//Ajouter articles dans un tableau
+var panier = [];
 
-let products = []
+function ajouterAuPanier (produit1) {
+    var nouvelElement; //Cette variable va stocker les détails du produit qui sera ajouter au panier
+    if (produit1 === 1) {
+            nouvelElement = {
+                id: 1,
+                nom: "Avocats",
+                prix: 2
+        };
+    } else if (produitId === 2) {
+        nouvelElement = {
+            id: 2,
+            nom: "Avocats",
+            prix: 4
+        };
 
-// openShopping.addEventListener('click', ()=> {
-//     header.classList.add('active');
-// })
-// fermerShopping.addEventListener('click', ()=> {
-//     main.classList.remove('active');
-// })
+        panier.push(nouvelElelemnt);
+
+        alert("Produit ajouter au panier !")
+}}
+
+//Afficher les produits dans le panier 
+ var PAnier = {}; // Cet objet 'panier' va stocker les infos sur les articles
+
+ function ajouterAuPanier(produit1, quantité) {
+    if (panier.hasOwnProperty(produit1)) { //verifie que le produit = déja dans panier
+        panier[produit1] += quantité; // Si oui, j'ajoute mon produit en plus
+    } else {
+        panier[produit1] = quantité; //sinon, je créer une nouvelle entrée pour le produit
+    }
+    document.getElementById('quantité').innerText = Object.values(panier).reduce((a,b)=> a + b, 0);
+ }
+ document.getElementById('quantité').innerText = Object.values(panier).reduce((a,b)=> a + b, 0);
+
+
+
 
 
 //Panier
@@ -48,3 +67,4 @@ let products = []
 //Calendrier_de_saison //
 
 //Connecter//
+
